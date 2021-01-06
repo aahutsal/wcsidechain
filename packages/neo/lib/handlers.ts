152f01edd05c3ed97447c5f75878ebbe9c601717
Arsen A. Gutsal <gutsal.arsen@gmail.com>
2021-01-06 08:33:54 +0200

    neo handler updated and renamed

---
+++ packages/neo/lib/handlers.ts
@@ -0,0 +1,31 @@
+"use strict"
+
+import fastify, { preHandlerAsyncHookHandler, preHandlerHookHandler, onRequestHookHandler } from 'fastify'
+import axios from 'axios'
+
+
+const getHandler = (handler: string, upstream: string) => {
+    const axiosInstance = axios.create({
+        baseURL: upstream,
+    })
+
+    let preHandler = async (req, res) => {
+        switch (req.url) {
+            case "/neo/getFeeees": {
+                /**
+                   Since we can't easily change `req.method` calling axios manually
+                   FIXME: this should be reworked
+                */
+                res = axiosInstance.post(req.path,
+                                   { "jsonrpc": "2.0", "id": 1, "method": "getblockcount", "params": [] })
+                    .then((response) => {
+                        res.send(response.data)
+                    })
+            }
+        }
+        return res;
+    }
+    let handlers = { preHandler }
+    return handlers[handler]
+}
+export { getHandler }
